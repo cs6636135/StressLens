44f5a7aaa42f26d410138002be59dd89e09a87ef
cs6636135 <s6604062636135@email.kmutnb.ac.th>
2026-02-27 07:28:06 +0700

    first commit

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,14 @@
+import supabase from "../config/supabase.js";
+async function verifyUser(req,res,next){
+    const token = req.headers.authorization?.split("")[1];
+    if(!token){
+        return res.status(401).json({ error: "No token"})
+    }
+    const {data, error} = await supabase.auth.getUser(token);
+    if(error){
+        return res.status(403).json({error: "Invalid token"});
+    }
+    req.user = data.user ;
+    next();
+}
+export default verifyUser;
